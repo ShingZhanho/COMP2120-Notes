8e6b92b7b9d619298fe71975b88b8ae913657ba9
Jacob Shing <zhanhoshing@icloud.com>
2025-03-19 10:07:21 +0800

    feat: add disclaimer header and script for automated inclusion in LaTeX documents
    @miktex

---
+++ build/add-disclaimer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Intended to be executed on GitHub Actions
+# Execute from root
+
+# Sets the flag INCLUDE_DISCLAIMER in preambles.tex to true
+echo "\\setbool{INCLUDE_DISCLAIMER}{true}" >> ./preambles.tex
