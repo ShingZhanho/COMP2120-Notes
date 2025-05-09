565895d8ab6df625f6c84b5bce37b2d3708e14cc
Jacob Shing <zhanhoshing@icloud.com>
2025-05-09 09:42:26 +0800

    gh, feat: add hash checking for LaTeX source files and download script

---
+++ build/download-hash.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This script downloads the hash of the LaTeX target.
+# ARGS:
+#   $1: The hash file's name (e.g. COMP2120-Notes.pdf.hash)
+#   $2: The local .tex sources hash.
+# OUTPUT:
+#   changed: 1 if the hash is different. 0 if the hash is the same.
+HASH_FILE="$1"
+HTTP_URL="https://shingzhanho.github.io/COMP2120-Notes/$HASH_FILE"
+HTTP_STATUS=$(curl -s -o ./$HASH_FILE -w "%{http_code}" "$HTTP_URL")
+if [ "$HTTP_STATUS" -ne 200 ]; then
+    echo "Warning: Failed to download hash file. HTTP status code: $HTTP_STATUS"
+    echo "Warning: Hash will be treated as 0."
+    echo "TARGET_HASH=0" >> "$GITHUB_ENV"
+else
+    # Read the hash from the file
+    TARGET_HASH=$(cat "$HASH_FILE")
+    echo "TARGET_HASH=$TARGET_HASH" >> "$GITHUB_ENV"
+    echo "Hash downloaded successfully: $TARGET_HASH"
+fi
+# Check if the hash is different from the local hash
+LOCAL_HASH="$2"
+if [ "$TARGET_HASH" != "$LOCAL_HASH" ]; then
+    echo "changed=1" >> "$GITHUB_OUTPUT"
+else
+    echo "changed=0" >> "$GITHUB_OUTPUT"
+fi
